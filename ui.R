e6b525a281d504b84720b18887248a092c2f089a
lboro-rdm <rdm@lboro.ac.uk>
2024-11-15 12:20:08 +0000

    complete revamp of the UI code

--- ui.R
+++ ui.R
@@ -1,48 +1,86 @@
 library(shiny)
 library(shinydashboard)
-library(shinyjs)
-library(pagedown)  # For PDF generation
 
 # Define UI for application
 ui <- fluidPage(
-  useShinyjs(),  # for dynamic UI actions
-  titlePanel("Scenario Planning"),
-  
+  titlePanel("Future Flight Scenario Toolkit"),
   fluidRow(
-    # Baseline Trends section
-    column(8, 
-           h3("Baseline Trends"),
+    column(9,
+           h4(style = "color:blue;", "Baseline Trends"),
            tags$ul(
-             tags$li(textOutput("baseline1")),  # Display baseline trend 1
-             tags$li(textOutput("baseline2"))   # Display baseline trend 2
+             tags$li(style = "color:blue;", textOutput("baseline1")),  # Display baseline trend 1
+             tags$li(style = "color:blue;", textOutput("baseline2"))   # Display baseline trend 2
            ),
-           
-           h3("Scenario Matrix"),
-           textOutput("driverTop"), # Scenario driver bottom
-           
-           # Arrange scenarioPlot and driverRight side by side
-           fluidRow(
-             # Left column for scenarioPlot
-             column(8,
-                    plotOutput("scenarioPlot", height = "300px")  # Placeholder for the 4-quadrant plot
-             ),
-             
-             # Right column for driverRight
-             column(4,
-                    textOutput("driverRight")  # Scenario driver left
-             )
-           )
     ),
-    
-    # User Inputs for Quadrants
-    column(4, 
-           h3("Write Scenarios for Each Quadrant"),
-           textInput("quad1", label = "Top Left Quadrant Scenario", value = ""),
-           textInput("quad2", label = "Top Right Quadrant Scenario", value = ""),
-           textInput("quad3", label = "Bottom Left Quadrant Scenario", value = ""),
-           textInput("quad4", label = "Bottom Right Quadrant Scenario", value = ""),
+    column(3,
+            downloadButton("downloadPDF", "Download PDF")
+           )
+  ),
+  fluidRow(
+    column(12,
+           p(),
+           h3("Scenario Matrix"),
+           div(style = "text-align: center;",
+               p(tags$strong(style = "color:red;", "Scenario driver")),
+               div(style = "color: red;", textOutput("driverTop")),
+              p()
+           )
+    )
+  ),
+  fluidRow(
+    column(3),
+    column(3,
+           textInput("Scenario1", 
+                     label = tags$label(style = "color: #D5006D;", "Scenario 1"), 
+                     value = "")
            
-           downloadButton("downloadPDF", "Download PDF")  # Button to download PDF
+    ),
+    column(3,
+           textInput("Scenario2", 
+                     label = tags$label(style = "color: #D5006D;", "Scenario 2"), 
+                     value = "")
+           
+    ),
+    column(3)
+  ),
+  fluidRow(
+    column(3,
+           div(style = "text-align: center;",
+               textInput("altDriverLeft", 
+                         label = tags$label(style = "color: green;", "Alternative Driver"), value = "")
+           )
+           ),
+    column(6),
+    column(3,
+           p(tags$strong(style = "color:red;", "Scenario driver")),
+           div(style = "color: red;", textOutput("driverRight")),
+           )
+  ),
+  fluidRow(
+    column(3),
+    column(3,
+           textInput("Scenario3", 
+                     label = tags$label(style = "color: #D5006D;", "Scenario 3"), 
+                     value = "")
+           
+    ),
+    column(3,
+           textInput("Scenario4", 
+                     label = tags$label(style = "color: #D5006D;", "Scenario 4"), 
+                     value = "")
+           
+    ),
+    column(3)
+  ),
+  fluidRow(
+    column(12,
+           div(style = "text-align: center;",
+               div(style = "display: inline-block; width:20%;",
+                   textInput("altDriverBottom", 
+                             label = tags$label(style = "color: green;", "Alternative Driver"), value = "")
+               )
+           )
     )
   )
 )
+
